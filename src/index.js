316651645ef4b66262f4521ad02918b715e1930c
jakwuh <jakwuh@yandex-team.ru>
2016-09-24 17:02:17 +0300

    Add Autobind to exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export {DecorateInstance} from './DecorateInstance';
+export {Autobind} from './Autobind';
 export {Defaults} from './Defaults';
 export {Throttle} from './Throttle';
 export {Debounce} from './Debounce';
